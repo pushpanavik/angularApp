8c6f05765336e2d3b50bae9c9e7296ebf2919d39
soma1701 <somasingh1701@gmail.com>
2018-04-06 11:33:40 +0530

    commit

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,3 +1,7 @@
-app.controller('homeCtrl',function($scope){
-
+app.controller('homeCtrl',function($scope,$timeout, $mdSidenav, $log){
+  $scope.toggleLeft = buildDelayedToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
 })
